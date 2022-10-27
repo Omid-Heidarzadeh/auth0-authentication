917d04652a4fa3ac22b5f3e3fd376aeabce9be88
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-10-27 20:37:27 +0330

    implement profile info page

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProfileInfo from './ProfileInfo';
 
-function Profile() {
-  return <div>Profile</div>;
+function Profile(props) {
+  const [{ profile, error }, setState] = useState({
+    profile: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    getUserProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getUserProfile() {
+    props.auth.getProfile((error, profile) => {
+      setState({ profile, error });
+    });
+  }
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {error ? (
+        <div>Error getting profile info: {error}</div>
+      ) : profile ? (
+        <ProfileInfo profile={profile} />
+      ) : (
+        <div>Loading profile...</div>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
