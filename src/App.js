3bab96d215aaeccc9404a3faf8849aa9a25bba4f
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-10-27 20:41:44 +0330

    Update: add authentication and react-router to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-function App() {
-  return <div className="App"></div>;
+import React, { useState } from 'react';
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import Auth from './Auth/Auth';
+import Callback from './Callback';
+import Nav from './Nav';
+import Home from './Home';
+import Profile from './Profile';
+import PageNotFound from './PageNotFound';
+import NavigateTo from './NavigateTo';
+
+function App(props) {
+  const navigate = useNavigate();
+  const [auth] = useState(new Auth(navigate));
+
+  return (
+    <div className="App">
+      <Nav auth={auth} />
+      <div className="body">
+        {
+          <Routes>
+            <Route path="/" element={<Home auth={auth} />} />
+            <Route path="callback" element={<Callback auth={auth} />} />
+            {
+              <Route
+                path="profile"
+                element={
+                  auth.isAuthenticated ? (
+                    <Profile auth={auth} />
+                  ) : (
+                    <NavigateTo to="/" />
+                  )
+                }
+              />
+            }
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        }
+      </div>
+    </div>
+  );
 }
 
 export default App;
