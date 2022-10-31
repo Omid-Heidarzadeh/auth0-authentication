81cfd1d46d7f5165065e047e1147ed3821d6f620
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-10-31 12:10:06 +0330

    refactor: show all profile info

--- src/ProfileInfo.js
+++ src/ProfileInfo.js
@@ -2,30 +2,42 @@ import React from 'react';
 
 function ProfileInfo(props) {
   const { profile } = props;
+  const { email, email_verified, picture, nickname, ...rest_profile_info } =
+    profile;
+  const verification = email_verified
+    ? { text: 'Verified', color: 'green' }
+    : { text: 'Not verified', color: 'red' };
+
   return (
     <div>
-      <img src={profile.picture} alt="user profile" />
+      <img src={picture} alt="user profile" />
       <table>
         <tbody>
           <tr>
             <td>Username:</td>
-            <td>{profile.nickname}</td>
+            <td>{nickname}</td>
           </tr>
           <tr>
             <td>Email:</td>
             <td>
               {
                 <span>
-                  {profile.email}{' '}
-                  {profile.email_verified ? (
-                    <span style={{ color: 'green' }}>(verified)</span>
-                  ) : (
-                    <span style={{ color: 'red' }}>(unverified)</span>
-                  )}
+                  {email}{' '}
+                  <span style={{ color: verification.color }}>
+                    {`(${verification.text})`}
+                  </span>
                 </span>
               }
             </td>
           </tr>
+          {Object.entries(rest_profile_info).map(([key, value]) => {
+            return (
+              <tr key={key}>
+                <td>{key} : </td>
+                <td>{value}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
