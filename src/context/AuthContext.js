07d05b89fa66591898550485defa76d638c6ce2f
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-11-03 02:12:30 +0330

    refactor: add AuthContextProvider which passes React-Router navigate function to auth instance

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Auth from '../Auth/Auth';
 
-export const auth = new Auth();
-export const AuthContext = React.createContext(auth);
+export const AuthContext = React.createContext();
+let _auth = new Auth();
+
+export function AuthContextProvider({ children }) {
+  const navigate = useNavigate();
+  _auth = new Auth(navigate);
+
+  return <AuthContext.Provider value={_auth}>{children}</AuthContext.Provider>;
+}
+
+export const auth = _auth;
