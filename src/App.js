b568a8e986ebe65c0d82d2014d668bb2159fd540
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-11-07 16:51:58 +0330

    feat: implement auth-renewToken in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import Nav from './Nav';
 import { AuthContextProvider, AuthContext } from './context/AuthContext';
@@ -8,12 +8,20 @@ function App(props) {
   const navigate = useNavigate();
   auth.setNavigate(navigate);
 
+  const [tokensRenewed, setTokensRenewed] = useState(false);
+
+  useEffect(() => {
+    auth.renewToken((err, sessionInfo) => {
+      if (sessionInfo) setTokensRenewed(true);
+    });
+  });
+
   return (
     <AuthContextProvider>
       <div className="App">
         <Nav />
         <div className="body">
-          <Outlet />
+          <Outlet context={{ tokensRenewed }} />
         </div>
       </div>
     </AuthContextProvider>
